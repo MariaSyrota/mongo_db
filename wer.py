9f5cde8a84096a518b7c5f9982ddeec1eca059b3
mary <123lalalala@ukr.net>
2023-10-24 19:01:09 +0300

    setup

--- wer.py
+++
@@ -1,8 +0,0 @@
-with open('qert.py', 'w') as json_file:
-    json.dump(data, json_file)
-data = [1, 2, 3, 4, 5]
-print(data_str)
-data = [1, 'hello', 2.5, True, 'world']
-integers = list(filter(lambda x: isinstance(x, int), data))
-
-print(integers)
